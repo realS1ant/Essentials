eaddfbd03f2c66e173be81030c775c22950e6067
realS1ant <trb.9900@gmail.com>
2020-10-21 23:37:37 -0500

    Fix saving

--- Essentials/src/com/earth2me/essentials/EditKitInventoryListener.java
+++ Essentials/src/com/earth2me/essentials/EditKitInventoryListener.java
@@ -1,15 +1,48 @@
 package com.earth2me.essentials;
 
+import com.earth2me.essentials.utils.DateUtil;
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import net.ess3.api.IEssentials;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.configuration.file.YamlConstructor;
+import org.bukkit.configuration.file.YamlRepresenter;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.representer.Representer;
 
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static com.earth2me.essentials.I18n.tl;
 
 public class EditKitInventoryListener implements Listener {
     private final transient IEssentials ess;
+    private static final String PASTE_URL = "https://hastebin.com/";
+    private static final String PASTE_UPLOAD_URL = PASTE_URL + "documents";
+    private static final Gson GSON = new Gson();
+
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final DumperOptions yamlOptions = new DumperOptions();
+    private final Representer yamlRepresenter = new YamlRepresenter();
+    private final YamlConstructor yamlConstructor = new YamlConstructor();
 
     public EditKitInventoryListener(final IEssentials ess) {
         this.ess = ess;
@@ -17,10 +50,125 @@ public class EditKitInventoryListener implements Listener {
 
     @EventHandler
     public void onInventoryClickEvent(final InventoryClickEvent event) {
+        if(event.getClickedInventory() == null) {
+            return;
+        }
         if (event.getView().getTitle().startsWith(ChatColor.GREEN + "" + ChatColor.BOLD + "Editing Kit " +
                 ChatColor.DARK_AQUA + "" + ChatColor.BOLD) && Objects.requireNonNull(event.getClickedInventory()).getHolder() == null) {
-            event.getWhoClicked().sendMessage("Triggered Click Event!");
-            event.setCancelled(true);
+            final String kitName = event.getView().getTitle().replace(ChatColor.GREEN + "" + ChatColor.BOLD + "Editing Kit " +
+                    ChatColor.DARK_AQUA + "" + ChatColor.BOLD, "");
+            final long delay = (int) ess.getKits().getKit(kitName).get("delay");
+            final User user = ess.getUser(event.getWhoClicked().getUniqueId());
+            if(event.getSlot() >= 45) {
+                event.setCancelled(true);
+                if(event.getSlot() == 48) {
+
+                    final ArrayList<ItemStack> contents = new ArrayList<>();
+                    for(ItemStack item : event.getClickedInventory().getStorageContents()) {
+                        if(item == null) {
+                            break;
+                        }
+                        if(item.getItemMeta() == null) {
+                            contents.add(item);
+                            break;
+                        }
+                        final String itemName = item.getItemMeta().getDisplayName();
+                        if(itemName == null) {
+                            contents.add(item);
+                            break;
+                        }
+
+                        System.out.println(tl("kitEditSpacerItemName", kitName) + tl("confirmKitEditItemName") + tl("cancelKitEditItemName"));
+
+                        if(!((itemName.equalsIgnoreCase(tl("kitEditSpacerItemName", kitName)))
+                                || (itemName.equalsIgnoreCase(tl("confirmKitEditItemName")))
+                                || (itemName.equalsIgnoreCase("cancelKitEditItemName")))) {
+                            contents.add(item);
+                        }
+                    }
+                    ess.getKits().removeKit(kitName);
+                    //ess.getKits().addKit(kitName, (List<String>) kit.get("items"), (int) kit.get("delay"));
+
+                    final List<String> list = new ArrayList<>();
+                    for (final ItemStack is : contents) {
+                        if (is != null && is.getType() != null && is.getType() != Material.AIR) {
+                            final String serialized = ess.getItemDb().serialize(is);
+                            list.add(serialized);
+                        }
+                    }
+                    // Some users might want to directly write to config knowing the consequences. *shrug*
+                    if (!ess.getSettings().isPastebinCreateKit()) {
+                        System.out.println("kitname: " + kitName + "\nlist: " + list + "\ndelay: " + delay);
+                        ess.getKits().addKit(kitName, list, delay);
+                        event.getWhoClicked().sendMessage(tl("updateKit", kitName));
+                        event.getWhoClicked().closeInventory();
+                    } else {
+                        final ConfigurationSection config = new MemoryConfiguration();
+                        config.set("kits." + kitName + ".delay", delay);
+                        config.set("kits." + kitName + ".items", list);
+
+                        final Yaml yaml = new Yaml(yamlConstructor, yamlRepresenter, yamlOptions);
+                        String fileContents = "# Copy the kit code below into the kits section in your config.yml file\n";
+                        fileContents += yaml.dump(config.getValues(false));
+
+                        uploadPaste(user.getSource(), kitName, delay, fileContents);
+                        event.getWhoClicked().sendMessage(tl("updateKit", kitName));
+                        event.getWhoClicked().closeInventory();
+                    }
+
+                } else if(event.getSlot() == 50) {
+                    event.getWhoClicked().closeInventory();
+                    event.getWhoClicked().sendMessage(tl("cancelKitEdit"));
+                }
+
+            }
         }
+    }
+
+    @EventHandler
+    public void inventoryCloseEvent(InventoryInteractEvent e) {
+        e.getWhoClicked().sendMessage(tl("cancelKitEdit"));
+    }
+
+    private void uploadPaste(final CommandSource sender, final String kitName, final long delay, final String contents) {
+        executorService.submit(() -> {
+            try {
+                final HttpURLConnection connection = (HttpURLConnection) new URL(PASTE_UPLOAD_URL).openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setRequestProperty("User-Agent", "EssentialsX plugin");
+                try (final OutputStream os = connection.getOutputStream()) {
+                    os.write(contents.getBytes(Charsets.UTF_8));
+                }
+                // Error
+                if (connection.getResponseCode() >= 400) {
+                    sender.sendMessage(tl("createKitFailed", kitName));
+                    final String message = CharStreams.toString(new InputStreamReader(connection.getErrorStream(), Charsets.UTF_8));
+                    ess.getLogger().severe("Error creating kit: " + message);
+                    return;
+                }
+
+                // Read URL
+                final JsonObject object = GSON.fromJson(new InputStreamReader(connection.getInputStream(), Charsets.UTF_8), JsonObject.class);
+                final String pasteUrl = PASTE_URL + object.get("key").getAsString();
+                connection.disconnect();
+
+                final String separator = tl("createKitSeparator");
+                String delayFormat = "0";
+                if (delay > 0) {
+                    delayFormat = DateUtil.formatDateDiff(System.currentTimeMillis() + (delay * 1000));
+                }
+                sender.sendMessage(separator);
+                sender.sendMessage(tl("createKitSuccess", kitName, delayFormat, pasteUrl));
+                sender.sendMessage(separator);
+                if (ess.getSettings().isDebug()) {
+                    ess.getLogger().info(sender.getSender().getName() + " created a kit: " + pasteUrl);
+                }
+            } catch (final Exception e) {
+                sender.sendMessage(tl("createKitFailed", kitName));
+                e.printStackTrace();
+            }
+        });
     }
 }
--- Essentials/src/com/earth2me/essentials/commands/Commandeditkit.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandeditkit.java
@@ -2,7 +2,13 @@ package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
 import com.earth2me.essentials.Kit;
+import com.earth2me.essentials.MetaItemStack;
+import com.earth2me.essentials.textreader.IText;
+import com.earth2me.essentials.textreader.KeywordReplacer;
+import com.earth2me.essentials.textreader.SimpleTextInput;
+import com.earth2me.essentials.utils.NumberUtil;
 import com.google.common.collect.Lists;
+import net.ess3.api.events.KitClaimEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -11,8 +17,10 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
 
 import static com.earth2me.essentials.I18n.tl;
 
@@ -36,11 +44,11 @@ public class Commandeditkit extends EssentialsCommand{
             }
 
             sender.sendMessage("editing kit here!!");
-            sender.getPlayer().openInventory(getKitInventory(kit));
+            sender.getPlayer().openInventory(getKitInventory(kit, sender));
         }
     }
 
-    public Inventory getKitInventory(Kit kit) {
+    public Inventory getKitInventory(Kit kit, CommandSource user) {
         final Inventory inv = Bukkit.getServer().createInventory(null, 54, ChatColor.GREEN + "" +
                 ChatColor.BOLD + "Editing Kit " + ChatColor.DARK_AQUA + "" + ChatColor.BOLD + kit.getName());
 
@@ -74,7 +82,7 @@ public class Commandeditkit extends EssentialsCommand{
         confirmItemMeta.setDisplayName(ChatColor.GREEN + "" + ChatColor.BOLD + "Save Kit");
         confirmItem.setItemMeta(confirmItemMeta);
 
-        inv.setItem(0, spacerItem);
+        /*inv.setItem(0, spacerItem);
         inv.setItem(1, spacerItem);
         inv.setItem(2, spacerItem);
         inv.setItem(3, spacerItem);
@@ -88,13 +96,13 @@ public class Commandeditkit extends EssentialsCommand{
         inv.setItem(17, spacerItem);
 
         inv.setItem(18, spacerItem);
-        inv.setItem(26, spacerItem);
+        inv.setItem(25, spacerItem);
 
         inv.setItem(27, spacerItem);
         inv.setItem(35, spacerItem);
 
         inv.setItem(36, spacerItem);
-        inv.setItem(44, spacerItem);
+        inv.setItem(44, spacerItem);*/
 
         inv.setItem(45, spacerItem);
         inv.setItem(46, spacerItem);
@@ -110,11 +118,160 @@ public class Commandeditkit extends EssentialsCommand{
         inv.setItem(52, spacerItem);
         inv.setItem(53, spacerItem);
 
-        for(ItemStack item : kit.)
-
-
+        //Add Items here
+        try {
+            addKitItems(inv, user, kit);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return inv;
+    }
+
+    public void addKitItems(Inventory inv, final CommandSource user, final Kit kit) throws Exception {
+        try {
+
+            final List<String> items = kit.getItems();
+            final IText input = new SimpleTextInput(items);
+            final IText output = new KeywordReplacer(input, user, ess, true, true);
+
+            final KitClaimEvent event = new KitClaimEvent(user.getUser(ess), kit);
+            Bukkit.getPluginManager().callEvent(event);
+            if (event.isCancelled()) {
+                return;
+            }
+
+            final boolean allowUnsafe = ess.getSettings().allowUnsafeEnchantments();
+            final boolean currencyIsSuffix = ess.getSettings().isCurrencySymbolSuffixed();
+            final List<ItemStack> itemList = new ArrayList<>();
+            final List<String> commandQueue = new ArrayList<>();
+            final List<String> moneyQueue = new ArrayList<>();
+            for (final String kitItem : output.getLines()) {
+                if (!currencyIsSuffix ? kitItem.startsWith(ess.getSettings().getCurrencySymbol()) : kitItem.endsWith(ess.getSettings().getCurrencySymbol())) {
+                    moneyQueue.add(NumberUtil.sanitizeCurrencyString(kitItem, ess));
+                    continue;
+                }
+
+                if (kitItem.startsWith("/")) {
+                    commandQueue.add(kitItem);
+                    continue;
+                }
+
+                final String[] parts = kitItem.split(" +");
+                final ItemStack parseStack = ess.getItemDb().get(parts[0], parts.length > 1 ? Integer.parseInt(parts[1]) : 1);
+
+                if (parseStack.getType() == Material.AIR) {
+                    continue;
+                }
+
+                final MetaItemStack metaStack = new MetaItemStack(parseStack);
+
+                if (parts.length > 2) {
+                    // We pass a null sender here because kits should not do perm checks
+                    metaStack.parseStringMeta(null, allowUnsafe, parts, 2, ess);
+                }
+
+                itemList.add(metaStack.getItemStack());
+            }
+
+            for(ItemStack item : itemList) {
+                inv.addItem(item);
+            }
+
+            final ArrayList<ItemStack> specialItems = new ArrayList<>();
+
+            for(String string : moneyQueue) {
+                final ItemStack moneyItem = new ItemStack(Material.PAPER, 1);
+                final ItemMeta moneyMeta = moneyItem.getItemMeta();
+                moneyMeta.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "Money Item");
+                final ArrayList<String> lore = new ArrayList<>();
+                lore.add(ChatColor.DARK_RED + "" + ChatColor.BOLD + string);
+                moneyMeta.setLore(lore);
+                moneyItem.setItemMeta(moneyMeta);
+                specialItems.add(moneyItem);
+            }
+
+            for(String string : commandQueue) {
+                final ItemStack commandItem = new ItemStack(Material.BOOK, 1);
+                final ItemMeta commandMeta = commandItem.getItemMeta();
+                commandMeta.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "Command Item");
+                final ArrayList<String> lore = new ArrayList<>();
+                lore.add(ChatColor.DARK_RED + "" + ChatColor.BOLD + string);
+                commandMeta.setLore(lore);
+                commandItem.setItemMeta(commandMeta);
+                specialItems.add(commandItem);
+            }
+
+            int slot = 36;
+            for(ItemStack item : specialItems) {
+                if(slot > 44) break;
+                if(inv.getItem(slot) != null) {
+                    slot++;
+                    break;
+                }
+                System.out.println(slot + " made it: " + item.toString());
+                inv.setItem(slot, item);
+                slot++;
+            }
+
+            /*
+            final Map<Integer, ItemStack> overfilled;
+            final boolean allowOversizedStacks = user.getUser(ess).isAuthorized("essentials.oversizedstacks");
+            final boolean isDropItemsIfFull = ess.getSettings().isDropItemsIfFull();
+            if (isDropItemsIfFull) {
+                if (allowOversizedStacks) {
+                    assert user.getPlayer() != null;
+                    overfilled = InventoryWorkaround.addOversizedItems(user.getPlayer().getInventory(), ess.getSettings().getOversizedStackSize(), itemList.toArray(new ItemStack[0]));
+                } else {
+                    assert user.getPlayer() != null;
+                    overfilled = InventoryWorkaround.addItems(user.getPlayer().getInventory(), itemList.toArray(new ItemStack[0]));
+                }
+                for (final ItemStack itemStack : overfilled.values()) {
+                    int spillAmount = itemStack.getAmount();
+                    if (!allowOversizedStacks) {
+                        itemStack.setAmount(Math.min(spillAmount, itemStack.getMaxStackSize()));
+                    }
+                    while (spillAmount > 0) {
+                        user.getWorld().dropItemNaturally(user.getLocation(), itemStack);
+                        spillAmount -= itemStack.getAmount();
+                    }
+                    spew = true;
+                }
+            } else {
+                if (allowOversizedStacks) {
+                    overfilled = InventoryWorkaround.addAllOversizedItems(user.getBase().getInventory(), ess.getSettings().getOversizedStackSize(), itemList.toArray(new ItemStack[0]));
+                } else {
+                    overfilled = InventoryWorkaround.addAllItems(user.getBase().getInventory(), itemList.toArray(new ItemStack[0]));
+                }
+                if (overfilled != null) {
+                    user.sendMessage(tl("kitInvFullNoDrop"));
+                    return false;
+                }
+            }
+            user.getBase().updateInventory();
+
+            // Process money & command queues
+            // Done after all items have been processed so commands are not run and money is not given if
+            // an error occurs during the item giving process
+            for (final String valueString : moneyQueue) {
+                final BigDecimal value = new BigDecimal(valueString.trim());
+                final Trade t = new Trade(value, ess);
+                t.pay(user, Trade.OverflowType.DROP);
+            }
+
+            for (final String cmd : commandQueue) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+            }
+
+            if (spew) {
+                user.sendMessage(tl("kitInvFull"));
+            }*/
+        } catch (final Exception e) {
+            user.getPlayer().updateInventory();
+            ess.getLogger().log(Level.WARNING, e.getMessage());
+            throw new Exception(tl("kitError2"), e);
+        }
+
     }
 
     @Override
